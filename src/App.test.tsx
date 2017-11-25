d8de56f0c315d6c8cb12f50050f005bd0bdbd292
Code Dude <dude@stokedbits.com>
2017-11-25 07:15:14 -0800

    Fixes tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('should render without throwing an error', function () {
+      expect(shallow(<App />).find('.App')).toHaveLength(1);
+  });
 });
